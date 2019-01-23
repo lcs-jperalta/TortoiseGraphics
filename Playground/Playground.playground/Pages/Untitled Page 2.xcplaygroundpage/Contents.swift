6d0d428c7151520317ff18fe093b5c39dc5bd928
Joven Peralta <jperalta@lcs.on.ca>
2019-01-22 21:23:34 -0500

    Moved the functions into extensions.swift.

--- Playground/Playground.playground/Pages/Untitled Page 2.xcplaygroundpage/Contents.swift
+++ Playground/Playground.playground/Pages/Untitled Page 2.xcplaygroundpage/Contents.swift
@@ -21,108 +21,19 @@ canvas.drawing { turtle in
     turtle.setHeading(90)
     
     // Uppercase J
-    // Face turtle for J
-    turtle.setHeading(325)
-    // Curve up
-    turtle.curve(withSides: 30, withSize: 15, drawSides: 9)
-    // Turn down
-    turtle.curve(withSides: 12, withSize: 4, drawSides: 4)
-    // Curve down
-    turtle.curve(withSides: 75, withSize: 100, drawSides: 2)
-    // Turn up
-    turtle.curve(withSides: 15, withSize: 5, drawSides: 7)
-    // Curve up
-    turtle.curve(withSides: 50, withSize: 17, drawSides: 5)
-    // Turn to next letter
-    turtle.curve(withSides: 90, withSize: 2, drawSides: 9)
-    // Set heading
-    turtle.setHeading(90)
+    turtle.uppercaseJ
     
     // Lowercase o before v
-    // Turn up
-    turtle.curve(withSides: -30, withSize: 5, drawSides: 6)
-    // Turn right
-    turtle.curve(withSides: 30, withSize: 9, drawSides: 1)
-    // Curve right
-    turtle.curve(withSides: 20, withSize: 7, drawSides: 6)
-    // Turn left
-    turtle.left(200)
-    // Curve down-left
-    turtle.curve(withSides: -20, withSize: 7, drawSides: 8)
-    // Curve right
-    turtle.curve(withSides: -25, withSize: 4, drawSides: 6)
-    // Curve up
-    turtle.curve(withSides: -30, withSize: 5, drawSides: 9)
-    // Curve left (for loop)
-    turtle.curve(withSides: -10, withSize: 7, drawSides: 4)
-    // Turn down (for loop)
-    turtle.curve(withSides: -10, withSize: 4, drawSides: 2)
-    // Curve right (loop)
-    turtle.curve(withSides: -10, withSize: 12, drawSides: 2)
-    // Turn right to next letter
-    turtle.curve(withSides: 6, withSize: 5, drawSides: 2)
-    // Set heading
-    turtle.setHeading(90)
+    turtle.lowercaseO()
     
     //Lowercase v after o
-    // Curve down
-    turtle.curve(withSides: 20, withSize: 4, drawSides: 4)
-    // Curve right
-    turtle.curve(withSides: 30, withSize: 4, drawSides: 4)
-    // Turn down
-    turtle.curve(withSides: -25, withSize: 2, drawSides: 7)
-    // Curve right then up
-    turtle.curve(withSides: -30, withSize: 3, drawSides: 8)
-    // Curve up
-    turtle.curve(withSides: -40, withSize: 15, drawSides: 2)
-    // Turn down
-    turtle.right(150)
-    // Curve right to next letter
-    turtle.curve(withSides: -30, withSize: 6, drawSides: 3)
-    // Set heading
-    turtle.setHeading(90)
+    turtle.lowercaseV()
     
     // Lowercase e after v
-    // Continue curve up
-    turtle.curve(withSides: -50, withSize: 1, drawSides: 10)
-    // Turn left
-    turtle.curve(withSides: -30, withSize: 1, drawSides: 10)
-    // Turn down
-    turtle.curve(withSides: -40, withSize: 1, drawSides: 9)
-    // Curve down
-    turtle.curve(withSides: -30, withSize: 8, drawSides: 3)
-    // Curve right
-    turtle.curve(withSides: -40, withSize: 3, drawSides: 7)
-    // Set heading
-    turtle.setHeading(90)
+    turtle.lowercaseE()
     
     // Lowercase n
-    // Turn up
-    turtle.curve(withSides: -35, withSize: 1, drawSides: 4)
-    // Curve up
-    turtle.curve(withSides: -35, withSize: 5, drawSides: 5)
-    // Turn right
-    turtle.curve(withSides: 40, withSize: 1, drawSides: 5)
-    // Curve right
-    turtle.curve(withSides: 40, withSize: 2, drawSides: 7)
-    // Turn down
-    turtle.curve(withSides: 40, withSize: 1, drawSides: 9)
-    // Go down
-    turtle.curve(withSides: 40, withSize: 26, drawSides: 1)
-    // Turn up
-    turtle.right(170)
-    // Curve up
-    turtle.curve(withSides: 40, withSize: 21, drawSides: 1)
-    // Curve right
-    turtle.curve(withSides: 40, withSize: 2, drawSides: 10)
-    // Turn down
-    turtle.curve(withSides: 40, withSize: 1, drawSides: 9)
-    // Go down
-    turtle.curve(withSides: 50, withSize: 14, drawSides: 1)
-    // Curve right
-    turtle.curve(withSides: -50, withSize: 1, drawSides: 18)
-    // Set heading
-    turtle.setHeading(90)
+    turtle.lowercaseN()
     
     // Go to last name
     turtle.setHeading(90)
@@ -132,25 +43,7 @@ canvas.drawing { turtle in
     turtle.setHeading(90)
     
     // Start uppercase P
-    // Turn right
-    turtle.right(20)
-    // Curve up
-    turtle.curve(withSides: -35, withSize: 4, drawSides: 12)
-    print("y position after curve up is \(turtle.ycor)")
-    // Turn down
-    turtle.left(165)
-    // Go down
-    turtle.curve(withSides: 200, withSize: 47, drawSides: 2)
-    // Turn up
-    turtle.right(175)
-    // Go up
-    turtle.curve(withSides: 150, withSize: 30, drawSides: 2)
-    // Turn right
-    turtle.curve(withSides: 50, withSize: 4, drawSides: 13)
-    // Turn down then left
-    turtle.curve(withSides: 40, withSize: 3, drawSides: 16)
-    // Curve left
-    turtle.curve(withSides: 35, withSize: 12, drawSides: 4)
+    turtle.uppercaseP()
     
     // Go to next letter
     turtle.setHeading(90)
@@ -159,86 +52,21 @@ canvas.drawing { turtle in
     turtle.forward(45)
     
     // Lowercase e
-    // Set heading
-    turtle.setHeading(90)
-    // Turn up
-    turtle.curve(withSides: -50, withSize: 4, drawSides: 13)
-    // Turn left
-    turtle.curve(withSides: -30, withSize: 1, drawSides: 9)
-    // Turn down
-    turtle.curve(withSides: -30, withSize: 2, drawSides: 6)
-    // Curve down
-    turtle.curve(withSides: -50, withSize: 8, drawSides: 3)
-    // Curve right
-    turtle.curve(withSides: -40, withSize: 1, drawSides: 10)
-    // Set heading
-    turtle.setHeading(90)
+    turtle.lowercasee()
     
     // Lowercase r
-    // Curve up
-    turtle.curve(withSides: -45, withSize: 4, drawSides: 13)
-    // Turn down
-    turtle.right(150)
-    // Curve right
-    turtle.curve(withSides: -35, withSize: 3, drawSides: 10)
-    // Turn down again
-    turtle.right(160)
-    // Curve down
-    turtle.curve(withSides: -50, withSize: 7, drawSides: 4)
-    // Turn right
-    turtle.curve(withSides: -30, withSize: 2, drawSides: 7)
-    // Set heading
-    turtle.setHeading(90)
+    turtle.lowercaseR()
     
     // Lowercase a
-    // Curve up
-      turtle.curve(withSides: -30, withSize: 5, drawSides: 7)
-    // Turn right
-      turtle.curve(withSides: 40, withSize: 3, drawSides: 9)
-    // Curve right
-      turtle.curve(withSides: 35, withSize: 9, drawSides: 2)
-    // Turn left
-      turtle.right(175)
-    // Curve left then down
-      turtle.curve(withSides: -45, withSize: 4, drawSides: 12)
-    // Curve right
-      turtle.curve(withSides: -40, withSize: 2, drawSides: 12)
-    // Turn up
-      turtle.curve(withSides: -45, withSize: 3, drawSides: 7)
-    // Curve up
-      turtle.curve(withSides: -40, withSize: 12, drawSides: 2)
-    // Turn down
-      turtle.right(190)
-    // Curve down
-      turtle.curve(withSides: -45, withSize: 13, drawSides: 2)
-    // Turn right
-      turtle.curve(withSides: -50, withSize: 1, drawSides: 14)
-    // Set heading
-      turtle.setHeading(90)
-    print("y position is \(turtle.ycor)")
+    turtle.lowercaseA()
     
     // Lowercase l
-    // Turn up
-      turtle.curve(withSides: -50, withSize: 5, drawSides: 10)
-    // Curve up
-      turtle.curve(withSides: -30, withSize: 40, drawSides: 2)
-    // Turn down
-      turtle.curve(withSides: -40, withSize: 2, drawSides: 18)
-    // Curve down
-      turtle.curve(withSides: -40, withSize: 23, drawSides: 4)
-    // Curve right
-      turtle.curve(withSides: -35, withSize: 4, drawSides: 8)
-    // Set heading
-      turtle.setHeading(90)
-    print("y position is \(turtle.ycor)")
+    turtle.lowercaseL()
     
     // Lowercase t
-    // Turn up
-      turtle.curve(withSides: -35, withSize: 4, drawSides: 8)
-    // Curve up
-      turtle.curve(withSides: -30, withSize: 100, drawSides: 1)
-    // Turn down
-      turtle.right(180)
-    turtle.curve(withSides: -30, withSize: 50, drawSides: 1)
+    turtle.lowercaseT()
+    
+    // Lowercase a
+    turtle.lowercaseA()
     
 }
--- Sources/Extensions.swift
+++ Sources/Extensions.swift
@@ -12,10 +12,215 @@ public extension Tortoise {
 
         self.penDown()
         for _ in 1...sideLimit {
-        self.forward(Double(size))
-        self.right(360 / Double(sideCount))
+            self.forward(Double(size))
+            self.right(360 / Double(sideCount))
         }
-    self.penUp()
+    }
+    func uppercaseJ() {
+        // Face self for J
+        self.setHeading(325)
+        // Curve up
+        self.curve(withSides: 30, withSize: 15, drawSides: 9)
+        // Turn down
+        self.curve(withSides: 12, withSize: 4, drawSides: 4)
+        // Curve down
+        self.curve(withSides: 75, withSize: 100, drawSides: 2)
+        // Turn up
+        self.curve(withSides: 15, withSize: 5, drawSides: 7)
+        // Curve up
+        self.curve(withSides: 50, withSize: 17, drawSides: 5)
+        // Turn to next letter
+        self.curve(withSides: 90, withSize: 2, drawSides: 9)
+        // Set heading
+        self.setHeading(90)
+    }
+    func lowercaseO() {
+        // Turn up
+        self.curve(withSides: -30, withSize: 5, drawSides: 6)
+        // Turn right
+        self.curve(withSides: 30, withSize: 9, drawSides: 1)
+        // Curve right
+        self.curve(withSides: 20, withSize: 7, drawSides: 6)
+        // Turn left
+        self.left(200)
+        // Curve down-left
+        self.curve(withSides: -20, withSize: 7, drawSides: 8)
+        // Curve right
+        self.curve(withSides: -25, withSize: 4, drawSides: 6)
+        // Curve up
+        self.curve(withSides: -30, withSize: 5, drawSides: 9)
+        // Curve left (for loop)
+        self.curve(withSides: -10, withSize: 7, drawSides: 4)
+        // Turn down (for loop)
+        self.curve(withSides: -10, withSize: 4, drawSides: 2)
+        // Curve right (loop)
+        self.curve(withSides: -10, withSize: 12, drawSides: 2)
+        // Turn right to next letter
+        self.curve(withSides: 6, withSize: 5, drawSides: 2)
+        // Set heading
+        self.setHeading(90)
+    }
+    func lowercaseV() {
+        // Curve down
+        self.curve(withSides: 20, withSize: 4, drawSides: 4)
+        // Curve right
+        self.curve(withSides: 30, withSize: 4, drawSides: 4)
+        // Turn down
+        self.curve(withSides: -25, withSize: 2, drawSides: 7)
+        // Curve right then up
+        self.curve(withSides: -30, withSize: 3, drawSides: 8)
+        // Curve up
+        self.curve(withSides: -40, withSize: 15, drawSides: 2)
+        // Turn down
+        self.right(150)
+        // Curve right to next letter
+        self.curve(withSides: -30, withSize: 6, drawSides: 3)
+        // Set heading
+        self.setHeading(90)
+    }
+    func lowercaseE() {
+        // Continue curve up
+        self.curve(withSides: -50, withSize: 1, drawSides: 10)
+        // Turn left
+        self.curve(withSides: -30, withSize: 1, drawSides: 10)
+        // Turn down
+        self.curve(withSides: -40, withSize: 1, drawSides: 9)
+        // Curve down
+        self.curve(withSides: -30, withSize: 8, drawSides: 3)
+        // Curve right
+        self.curve(withSides: -40, withSize: 3, drawSides: 7)
+        // Set heading
+        self.setHeading(90)
+    }
+    func lowercaseN() {
+        // Turn up
+        self.curve(withSides: -35, withSize: 1, drawSides: 4)
+        // Curve up
+        self.curve(withSides: -35, withSize: 5, drawSides: 5)
+        // Turn right
+        self.curve(withSides: 40, withSize: 1, drawSides: 5)
+        // Curve right
+        self.curve(withSides: 40, withSize: 2, drawSides: 7)
+        // Turn down
+        self.curve(withSides: 40, withSize: 1, drawSides: 9)
+        // Go down
+        self.curve(withSides: 40, withSize: 26, drawSides: 1)
+        // Turn up
+        self.right(170)
+        // Curve up
+        self.curve(withSides: 40, withSize: 21, drawSides: 1)
+        // Curve right
+        self.curve(withSides: 40, withSize: 2, drawSides: 10)
+        // Turn down
+        self.curve(withSides: 40, withSize: 1, drawSides: 9)
+        // Go down
+        self.curve(withSides: 50, withSize: 14, drawSides: 1)
+        // Curve right
+        self.curve(withSides: -50, withSize: 1, drawSides: 18)
+        // Set heading
+        self.setHeading(90)
+    }
+    func uppercaseP() {
+        // Turn right
+        self.right(20)
+        // Curve up
+        self.curve(withSides: -35, withSize: 4, drawSides: 12)
+        // Turn down
+        self.left(165)
+        // Go down
+        self.curve(withSides: 200, withSize: 47, drawSides: 2)
+        // Turn up
+        self.right(175)
+        // Go up
+        self.curve(withSides: 150, withSize: 30, drawSides: 2)
+        // Turn right
+        self.curve(withSides: 50, withSize: 4, drawSides: 13)
+        // Turn down then left
+        self.curve(withSides: 40, withSize: 3, drawSides: 16)
+        // Curve left
+        self.curve(withSides: 35, withSize: 12, drawSides: 4)
+    }
+    func lowercasee() {
+        // Set heading
+        self.setHeading(90)
+        // Turn up
+        self.curve(withSides: -50, withSize: 4, drawSides: 13)
+        // Turn left
+        self.curve(withSides: -30, withSize: 1, drawSides: 9)
+        // Turn down
+        self.curve(withSides: -30, withSize: 2, drawSides: 6)
+        // Curve down
+        self.curve(withSides: -50, withSize: 8, drawSides: 3)
+        // Curve right
+        self.curve(withSides: -40, withSize: 1, drawSides: 10)
+        // Set heading
+        self.setHeading(90)
+    }
+    func lowercaseR() {
+        // Curve up
+        self.curve(withSides: -45, withSize: 4, drawSides: 13)
+        // Turn down
+        self.right(150)
+        // Curve right
+        self.curve(withSides: -35, withSize: 3, drawSides: 10)
+        // Turn down again
+        self.right(160)
+        // Curve down
+        self.curve(withSides: -50, withSize: 7, drawSides: 4)
+        // Turn right
+        self.curve(withSides: -30, withSize: 2, drawSides: 7)
+        // Set heading
+        self.setHeading(90)
+    }
+    func lowercaseA() {
+        // Curve up
+        self.curve(withSides: -30, withSize: 5, drawSides: 7)
+        // Turn right
+        self.curve(withSides: 40, withSize: 3, drawSides: 9)
+        // Curve right
+        self.curve(withSides: 35, withSize: 9, drawSides: 2)
+        // Turn left
+        self.right(175)
+        // Curve left then down
+        self.curve(withSides: -45, withSize: 4, drawSides: 12)
+        // Curve right
+        self.curve(withSides: -40, withSize: 2, drawSides: 12)
+        // Turn up
+        self.curve(withSides: -45, withSize: 3, drawSides: 7)
+        // Curve up
+        self.curve(withSides: -40, withSize: 12, drawSides: 2)
+        // Turn down
+        self.right(190)
+        // Curve down
+        self.curve(withSides: -45, withSize: 13, drawSides: 2)
+        // Turn right
+        self.curve(withSides: -50, withSize: 1, drawSides: 14)
+        // Set heading
+        self.setHeading(90)
+    }
+    func lowercaseL() {
+        // Turn up
+        self.curve(withSides: -50, withSize: 5, drawSides: 10)
+        // Curve up
+        self.curve(withSides: -30, withSize: 40, drawSides: 2)
+        // Turn down
+        self.curve(withSides: -40, withSize: 2, drawSides: 18)
+        // Curve down
+        self.curve(withSides: -40, withSize: 23, drawSides: 4)
+        // Curve right
+        self.curve(withSides: -35, withSize: 4, drawSides: 8)
+        // Set heading
+        self.setHeading(90)
+    }
+    func lowercaseT() {
+        // Turn up
+        self.curve(withSides: -35, withSize: 4, drawSides: 8)
+        // Curve up
+        self.curve(withSides: -30, withSize: 100, drawSides: 1)
+        // Turn down
+        self.right(180)
+        // Curve down
+        self.curve(withSides: -30, withSize: 50, drawSides: 1)
     }
     func hairball(withXcor: Double, withYcor: Double, withScale: Int) {
         for _ in 1...15 {
